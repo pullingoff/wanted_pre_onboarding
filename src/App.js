e72114cfef7d58b17cec4a2ef5464c84aad5bc6c
Haeun Park <50111853+pullingoff@users.noreply.github.com>
2022-04-16 20:39:22 +0900

    :a: first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import './App.css';
 import Toggle from './components/Toggle';
 import Tab from './components/Tab';
 import Input from './components/Input';
 import Dropdown from "./components/Dropdown"
+import styled from 'styled-components';
 
-function App() {
+const App = () => {
   return (
     <>
+    <CompContainer>
       <h1>Toggle.js</h1>
       <Toggle/>
-
+    </CompContainer>
+    <CompContainer>
       <h1>Tab.js</h1>
       <Tab/>
-
+    </CompContainer>
+    <CompContainer>
       <h1>Input.js</h1>
       <Input/>
-
+    </CompContainer>
+    <CompContainer>
       <h1>Dropdown.js</h1>
       <Dropdown />
+    </CompContainer>
     </>
   );
 }
+
+const CompName = styled.h1`
+`
+
+const CompContainer = styled.section`
+padding: 10px;
+`
 
 export default App;
